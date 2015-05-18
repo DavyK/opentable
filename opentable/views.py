c8144c8a87c03361c91697ab416f40099cf6d597
Davy <davykavanagh@gmail.com>
2015-05-18 01:08:17 -0400

    added natural times to recent items

--- opentable/views.py
+++ opentable/views.py
@@ -19,8 +19,6 @@ from opentable.forms import LoginForm, CustomUserCreationForm, ChangePasswordFor
 
 def home(request):
 
-
-
     return render_to_response('opentable/home.html', {}, context_instance=RequestContext(request))
 
 
