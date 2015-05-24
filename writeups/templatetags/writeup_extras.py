050d36086c617ac6f8845c7f5d5dd3874dde0e01
Davy <davykavanagh@gmail.com>
2015-05-24 04:37:31 -0400

    Minor fix

--- writeups/templatetags/writeup_extras.py
+++ writeups/templatetags/writeup_extras.py
@@ -9,4 +9,5 @@ def replace_linebr(value):
     """Replaces all values of line break from the given string with a line space."""
     value = value.replace("<p>", "")
     value = value.replace("</p>", " ")
+    value = value.replace("<br />", " ")
     return value
