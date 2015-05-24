eb14b0a3d476125808539c4b92e3c9ca2be7f50b
Davy <davykavanagh@gmail.com>
2015-05-24 12:20:28 -0400

    Minor fix

--- static/js/custom.js
+++ static/js/custom.js
@@ -8,4 +8,10 @@ $(document).ready(function(){
         $('#writeup-search-form').attr("action", "/writeups/listWriteups/oldest/");
         $('#writeup-search-form').submit();
     });
+    $('#reverse-page-order').on('click', function(){
+        var writeups = $('#to-be-sorted').children().get().reverse();
+        for(var i = 0; i < writeups.length; i++){
+            writeups[i].parentNode.appendChild(writeups[i])
+        }
+    })
 });
