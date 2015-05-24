ad1d3c8c5b55546af7b57183b5bfc5592dd4aa1f
Davy <davykavanagh@gmail.com>
2015-05-24 04:34:52 -0400

    Minor fix

--- writeups/templatetags/writeup_extras.py
+++ writeups/templatetags/writeup_extras.py
@@ -7,4 +7,6 @@ register = template.Library()
 @register.filter(name='replace_linebr')
 def replace_linebr(value):
     """Replaces all values of line break from the given string with a line space."""
-    return value.replace("<br />", ' ')
+    value = value.replace("<p>", "")
+    value = value.replace("</p>", " ")
+    return value
